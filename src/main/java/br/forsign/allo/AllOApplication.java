483a68a3f5a5b0caa7eca756713352892ff5b0e6
M.O <prandini.oliveira@gmail.com>
2024-02-23 23:19:52 -0300

    feat: commit inicial

---
+++ src/main/java/br/forsign/allo/AllOApplication.java
@@ -0,0 +1,13 @@
+package br.forsign.allo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllOApplication.class, args);
+	}
+
+}
