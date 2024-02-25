92c5e655515dd13db3a41f71c92e789bd2e903cd
kaio.prandini <prandini.kaio@gmail.com>
2024-02-25 13:55:47 -0300

    feat: adicionado estrutura de projeto e base end-points do usuario

--- src/main/java/br/forsign/allo/user/domain/User.java
+++ src/main/java/br/forsign/allo/user/domain/User.java
@@ -1,11 +1,15 @@
 package br.forsign.allo.user.domain;
 
-import br.forsign.allo.contact.domain.Contacts;
+import br.forsign.allo.contact.domain.Contact;
 import br.forsign.allo.document.domain.Documents;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
 
 @Entity
 @Table(name = "USUARIO")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,8 +17,8 @@ public class User {
     private Long id;
     @Column(name = "USER_NAME")
     private String name;
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     private Documents documents;
-    @OneToOne
-    private Contacts contacts;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Contact contacts;
 }
--- src/main/java/br/forsign/allo/user/repository/UserRepository.java
+++ src/main/java/br/forsign/allo/user/repository/UserRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    //@Query("")
-   //User findAllByName(String name);
-
 }
