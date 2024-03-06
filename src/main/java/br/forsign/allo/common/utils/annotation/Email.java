cf640d3d4285de4b7ac72aa3a4357e8aa6bfb2b7
kaio.prandini <prandini.kaio@gmail.com>
2024-03-05 21:58:53 -0300

    feat: Annotation para validar inputs de Emails

---
+++ src/main/java/br/forsign/allo/common/utils/annotation/Email.java
@@ -0,0 +1,19 @@
+package br.forsign.allo.common.utils.annotation;
+
+import br.forsign.allo.common.utils.annotation.validator.CpfCnpjValidator;
+import br.forsign.allo.common.utils.annotation.validator.EmailValidator;
+import br.forsign.allo.user.domain.TipoDocumento;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = {EmailValidator.class})
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Email {
+    String message() default "E-mail inválido";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
