ce60f500e233bf43c669647aae63a6e188f978a4
Prandini <prandini.kaio@gmail.com>
2024-05-23 22:42:57 -0300

    feat: refactor de logica de negocio, baseado em entidade

--- src/main/java/br/forsign/allo/entidade/model/EntidadeInput.java
+++ src/main/java/br/forsign/allo/entidade/model/EntidadeInput.java
@@ -1,8 +1,25 @@
 package br.forsign.allo.entidade.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
 /*
  * @author prandini
  * created 5/22/24
  */
+
+@Data
 public class EntidadeInput {
+
+    @Schema(title = "Identificador", example = "3")
+    private Long id;
+
+    @Schema(title = "Email", example = "example@org.com")
+    private String email;
+
+    @Schema(title = "Telefone", example = "+55 33 978890000")
+    private String telefone;
+
+    @Schema(title = "CPF CNPJ", example = "000.000.000-00")
+    private String cpfCnpj;
 }
