4559c2ba85e2628ad66fbcb4b94c9ffe4dbcf60f
Prandini <prandini.kaio@gmail.com>
2024-05-24 18:01:26 -0300

    chore: adicionado annotations para validacao do input

--- src/main/java/br/forsign/allo/entidade/model/EntidadeInput.java
+++ src/main/java/br/forsign/allo/entidade/model/EntidadeInput.java
@@ -1,6 +1,9 @@
 package br.forsign.allo.entidade.model;
 
+import br.forsign.allo.common.utils.annotation.CpfCnpj;
+import br.forsign.allo.user.domain.TipoPessoa;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 /*
@@ -15,11 +18,14 @@ public class EntidadeInput {
     private Long id;
 
     @Schema(title = "Email", example = "example@org.com")
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String email;
 
     @Schema(title = "Telefone", example = "+55 33 978890000")
+    @Pattern(regexp = "^(\\d{2,3})(\\d{9})$")
     private String telefone;
 
     @Schema(title = "CPF CNPJ", example = "000.000.000-00")
+    @CpfCnpj(tipoDocumento = TipoPessoa.FISICA_JURIDICA)
     private String cpfCnpj;
 }
