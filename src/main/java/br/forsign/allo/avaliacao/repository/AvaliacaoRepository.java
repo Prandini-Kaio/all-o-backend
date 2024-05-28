aa65d506e7a549df3c3171680fdb58789be7f2fc
Prandini <prandini.kaio@gmail.com>
2024-05-28 19:19:05 -0300

    feat: avaliacao em destaque

--- src/main/java/br/forsign/allo/avaliacao/repository/AvaliacaoRepository.java
+++ src/main/java/br/forsign/allo/avaliacao/repository/AvaliacaoRepository.java
@@ -8,10 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
 
     @Query("SELECT a FROM Avaliacao a JOIN a.provedor p WHERE p.id = :id")
     Page<Avaliacao> byProvedor(Long id, Pageable pageable);
+
+    @Query("SELECT a FROM Avaliacao a JOIN a.provedor p WHERE p.id = :id")
+    Stream<Avaliacao> findByProvedor(Long id);
 
 }
