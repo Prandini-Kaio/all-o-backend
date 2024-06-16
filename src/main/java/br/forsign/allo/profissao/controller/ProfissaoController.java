be16377c74d4dfd873f151779d451314343fb400
Prandini <prandini.kaio@gmail.com>
2024-06-16 20:15:24 -0300

    sync

--- src/main/java/br/forsign/allo/profissao/controller/ProfissaoController.java
+++ src/main/java/br/forsign/allo/profissao/controller/ProfissaoController.java
@@ -11,12 +11,16 @@ import br.forsign.allo.profissao.service.ProfissaoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
