975bfb7a61c26ea669ede9bc247cf20bbd458a50
Nogs0 <joaoguinogueira04@gmail.com>
2024-06-18 21:11:11 -0300

    alterando dtos do Cliente e criando alguns métodos

--- src/main/java/br/forsign/allo/profissao/controller/ProfissaoController.java
+++ src/main/java/br/forsign/allo/profissao/controller/ProfissaoController.java
@@ -9,12 +9,14 @@ import br.forsign.allo.profissao.model.ProfissaoInput;
 import br.forsign.allo.profissao.model.ProfissaoOutput;
 import br.forsign.allo.profissao.model.ProfissaoSugestaoInput;
 import br.forsign.allo.profissao.service.ProfissaoService;
+import br.forsign.allo.provedor.model.ProvedorOutput;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -39,6 +43,15 @@ public class ProfissaoController {
             description = "Consulta todas as profissões cadastradas no sistema.")
     public ResponseEntity<Page<ProfissaoOutput>> findAll(Pageable pageable){
         return ResponseEntity.ok().body(this.service.findAll(pageable));
+    }
+
+    @GetMapping("/filter")
+    @Operation(
+            summary = "Retorna todas as profissões com base em um filtro.",
+            description = "Retorna todos os prestadores ativos com base em um filtro."
+    )
+    public ResponseEntity<List<ProfissaoOutput>> getByFilter(@RequestParam String profissao){
+        return ResponseEntity.ok().body(service.findByFilter(profissao));
     }
 
     @GetMapping("/{id}")
