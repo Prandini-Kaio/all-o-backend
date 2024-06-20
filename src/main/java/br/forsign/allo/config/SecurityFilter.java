25c18fdc24edb4e1b952ea2995751af37f405c0e
Prandini <prandini.kaio@gmail.com>
2024-06-20 14:54:10 -0300

    feat: adicionado login e autenticacao

---
+++ src/main/java/br/forsign/allo/config/SecurityFilter.java
@@ -0,0 +1,51 @@
+package br.forsign.allo.config;
+
+import br.forsign.allo.usuario.repository.UsuarioRepository;
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * @author kaiooliveira
+ * created 20/06/2024
+ */
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Resource
+    private UsuarioRepository usuarioRepository;
+
+    @Resource
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if(token != null){
+            String login = tokenService.validateToken(token);
+            UserDetails user = usuarioRepository.findByLogin(login);
+
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        return authHeader == null ? authHeader : authHeader.replace("Bearer ", "");
+    }
+}
