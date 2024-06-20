2af4d6aa5c534b1697d80eec8d75d8b15b42ee88
Prandini <prandini.kaio@gmail.com>
2024-06-20 14:54:10 -0300

    feat: adicionado login e autenticacao

---
+++ src/main/java/br/forsign/allo/auth/model/LoginOutput.java
@@ -0,0 +1,13 @@
+package br.forsign.allo.auth.model;
+
+import lombok.Data;
+
+/**
+ * @author kaiooliveira
+ * created 20/06/2024
+ */
+@Data
+public class LoginOutput {
+
+    private String token;
+}
