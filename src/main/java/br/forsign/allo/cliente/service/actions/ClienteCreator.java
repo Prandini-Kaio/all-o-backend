08c240aae9290e4547777557114789e2c97e392c
Nogs0 <joaoguinogueira04@gmail.com>
2024-06-23 04:57:13 -0300

    alteracoes servicos

--- src/main/java/br/forsign/allo/cliente/service/actions/ClienteCreator.java
+++ src/main/java/br/forsign/allo/cliente/service/actions/ClienteCreator.java
@@ -40,7 +40,7 @@ public class ClienteCreator {
 
     public Cliente create(ClienteCadastroInput clienteInput){
         logger.info("Criando um novo cliente {}.", clienteInput.getCliente().getNome());
-git
+
         ClienteInput input = clienteInput.getCliente();
 
         validator.validarCreate(input);
--- src/main/java/br/forsign/allo/servico/service/action/ServicoGetter.java
+++ src/main/java/br/forsign/allo/servico/service/action/ServicoGetter.java
@@ -1,10 +1,18 @@
 package br.forsign.allo.servico.service.action;
 
+import br.forsign.allo.auth.service.AuthService;
+import br.forsign.allo.cliente.domain.Cliente;
+import br.forsign.allo.cliente.service.actions.ClienteGetter;
 import br.forsign.allo.common.utils.CommonExceptionSupplier;
+import br.forsign.allo.provedor.domain.Provedor;
+import br.forsign.allo.provedor.service.action.ProvedorGetter;
 import br.forsign.allo.servico.domain.Servico;
-import br.forsign.allo.servico.service.repository.ServicoRepository;
+import br.forsign.allo.servico.repository.ServicoRepository;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author kaiooliveira
@@ -16,8 +24,23 @@ public class ServicoGetter {
 
     @Resource
     private ServicoRepository repository;
+    @Autowired
+    private ProvedorGetter provedorGetter;
+    @Autowired
+    private ClienteGetter clienteGetter;
 
     public Servico byId(Long id){
         return repository.findById(id).orElseThrow(CommonExceptionSupplier.naoEncontrado("Servico", id));
     }
+
+    public List<Servico> byNaoVistoPeloProvedor() {
+        Provedor provedor = provedorGetter.byUsername(AuthService.getContextUser().getUsername());
+        return repository.findByNaoVistoPeloProvedor(provedor.getId());
+    }
+
+    public List<Servico> byNaoVistoPeloCliente() {
+        Cliente cliente = clienteGetter.byUsername(AuthService.getContextUser().getUsername());
+        return repository.findByNaoVistoPeloCliente(cliente.getId());
+    }
+
 }
