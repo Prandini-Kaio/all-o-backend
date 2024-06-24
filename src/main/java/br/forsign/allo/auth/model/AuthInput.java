9070734af512067f267d8463f7dbb7bdce487677
Prandini <prandini.kaio@gmail.com>
2024-06-24 20:07:34 -0300

    feat: Validacao email e senha minima para cadastro

--- src/main/java/br/forsign/allo/auth/model/AuthInput.java
+++ src/main/java/br/forsign/allo/auth/model/AuthInput.java
@@ -1,7 +1,9 @@
 package br.forsign.allo.auth.model;
 
+import br.forsign.allo.common.utils.annotation.Email;
 import br.forsign.allo.provedor.model.ProvedorInput;
 import br.forsign.allo.usuario.domain.UsuarioRole;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 /**
@@ -12,8 +14,10 @@ import lombok.Data;
 @Data
 public class AuthInput {
 
+    @Email
     private String login;
 
+    @Size(min = 8, message = "O tamanho minímo da senha é de 8 caracteres.")
     private String senha;
 
     private UsuarioRole role;
