3a83be9a7063fe7eeb3e46012f5b47a1d37eca99
Dudu <dudupassoni@gmail.com>
2024-06-23 23:13:58 -0300

    .

---
+++ src/main/java/br/forsign/allo/provedor/model/ProvedorDestaquesOutput.java
@@ -0,0 +1,17 @@
+package br.forsign.allo.provedor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProvedorDestaquesOutput {
+    private Long id;
+
+    private String razaoSocial;
+    
+}
