8948956136ac02f70ca9fdb52f7dc6d03b52c0a2
Prandini <prandini.kaio@gmail.com>
2024-06-23 21:37:32 -0300

    fix: alterado perfilInput

--- src/main/java/br/forsign/allo/provedor/converter/PerfilProvedorMapper.java
+++ src/main/java/br/forsign/allo/provedor/converter/PerfilProvedorMapper.java
@@ -12,7 +12,6 @@ public interface PerfilProvedorMapper {
     @Mapping(source = "totalAvaliacao", target = "totalAvaliacoes")
     PerfilProvedorOutput toOutput(PerfilProvedor perfilProvedor);
 
-    @Mapping(source = "perfilProvedor.id", target = "idAvaliacao")
     PerfilProvedorInput toInput(PerfilProvedor perfilProvedor);
 
 }
