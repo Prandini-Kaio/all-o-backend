4bc0889742fe0f95922f9e0d8685607c1d7149eb
Nogs0 <joaoguinogueira04@gmail.com>
2024-10-29 21:35:07 -0300

    ajuste SecurityConfig adicionando role do CLIENTE

--- src/main/java/br/forsign/allo/config/SecurityConfig.java
+++ src/main/java/br/forsign/allo/config/SecurityConfig.java
@@ -36,8 +36,8 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/servico/abertura/**").hasRole("CLIENTE")
-                        .requestMatchers("/provedor").hasRole("PROVEDOR")
-                        .requestMatchers("/cliente").hasRole("CLIENTE")
+                        .requestMatchers("/provedor").hasAnyRole("PROVEDOR", "CLIENTE")
+                        .requestMatchers("/cliente").hasRole("CLIENTE"
 
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/auth/register").permitAll()
